8f735d07519d911d8874bc24196b25d2b7ad84a9
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2023-04-07 16:55:18 -0300

    chore: inital config

---
+++ src/styles/theme.ts
@@ -0,0 +1,45 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      900: '#000000',
+      800: '#131419',
+      700: '#828282',
+      600: '#CFCFCF',
+      500: '#BDBDBD',
+      300: '#E0E0E0',
+      200: '#F2F2F2', 
+      100: '#F8F9FB',
+      50: '#FFFFFF', 
+    },
+    purple: {
+      900: '#2B076E',
+      800: '#491F98',
+      300: '#9E81AD',
+    },
+  },
+  fonts: {
+    heading: 'Rubik',
+    body: 'Rubik',
+  },
+  styles: {
+    global: {
+      'html::-webkit-scrollbar': {
+        bg: 'blackAlpha.300',
+        w: '2',
+      },
+      'html::-webkit-scrollbar-thumb': {
+        background: 'blue.500',
+        borderRadius: '5px',
+      },
+      body: {
+        bg: 'gray.800',
+        color: 'gray.50',
+      },
+      '*': {
+        scrollBehavior: 'smooth',
+      },
+    },
+  },
+});
